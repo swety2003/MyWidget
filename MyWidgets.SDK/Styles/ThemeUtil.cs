207d008e9d671f0068451d613e3d222b9f38da9c
SwetyCore <swety2003@outlook.com>
2023-03-24 10:56:39 +0800

    添加DynamicColor

--- MyWidgets.SDK/Styles/ThemeUtil.cs
+++ MyWidgets.SDK/Styles/ThemeUtil.cs
@@ -4,56 +4,64 @@ using MaterialColorUtilities.Utils;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using Color = System.Windows.Media.Color;
+using ColorConverter = System.Windows.Media.ColorConverter;
 using DColor = System.Drawing.Color;
+
 namespace MyWidgets.SDK.Styles
 {
-    internal class ThemeUtil
+    public static class ThemeUtil
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool SystemParametersInfo(uint uAction, uint uParam, StringBuilder lpvParam, uint init);
+        const uint SPI_GETDESKWALLPAPER = 0x0073;  
 
-        private static uint[] CreateResizedImage(ImageSource source, int width=200, int height=200, int margin=0)
+
+        public static Bitmap resizeImage(Image imgToResize, System.Drawing.Size size)
         {
-            var rect = new Rect(margin, margin, width - margin * 2, height - margin * 2);
-
-            var group = new DrawingGroup();
-            RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.HighQuality);
-            group.Children.Add(new ImageDrawing(source, rect));
-
-            var drawingVisual = new DrawingVisual();
-            using (var drawingContext = drawingVisual.RenderOpen())
-                drawingContext.DrawDrawing(group);
-
-            var resizedImage = new RenderTargetBitmap(
-                width, height,         // Resized dimensions
-                96, 96,                // Default DPI values
-                PixelFormats.Default); // Default pixel format
-            resizedImage.Render(drawingVisual);
-
-            var img = BitmapFrame.Create(resizedImage);
-
-            var arr = new uint[200*200*8];
-            return arr;
+            return new Bitmap(imgToResize, size);
         }
 
+        public static int[] GetPixels(this Bitmap img)
+        {
+            var ret = new List<int>();
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    ret.Add(img.GetPixel(i, j).ToArgb());
+                }
+            }
+            return ret.ToArray();
+        }
 
         public static Scheme<Color> Create(string source,bool isdark)
         {
-
+            StringBuilder wallPaperPath = new StringBuilder(200);
+            if (SystemParametersInfo(SPI_GETDESKWALLPAPER, 200, wallPaperPath, 0))
+            {
+                source = wallPaperPath.ToString();
+            }
             //uint[] pixels = CreateResizedImage(source);
-            SKBitmap bitmap;
+            //SKBitmap bitmap;
+            Bitmap bitmap;
             using (FileStream fs = new FileStream(source, FileMode.Open))
             {
-
-                bitmap = SKBitmap.Decode(fs).Resize(new SKImageInfo(112, 112), SKFilterQuality.Medium);
+                bitmap = resizeImage(Image.FromStream(fs),new System.Drawing.Size(112,112));
             }
 
-            uint[] pixels = Array.ConvertAll(bitmap.Pixels, p => (uint)p);
+
+
+            uint[] pixels = Array.ConvertAll(bitmap.GetPixels(), p => (uint)p);
             // This is where the magic happens
             uint seedColor = ImageUtils.ColorsFromImage(pixels).First();
 
@@ -67,9 +75,6 @@ namespace MyWidgets.SDK.Styles
             Scheme<uint> lightScheme = new LightSchemeMapper().Map(corePalette);
             Scheme<uint> darkScheme = new DarkSchemeMapper().Map(corePalette);
 
-            // Easily convert between Schemes with different color types
-            //Scheme<DColor> lightSchemeColor = lightScheme.Convert(x => DColor.FromArgb((int)x));
-            //Scheme<DColor> darkSchemeColor = darkScheme.Convert(x => DColor.FromArgb((int)x));
 
             Scheme<Color> darkSchemeColor = darkScheme.Convert(x => (Color)ColorConverter.ConvertFromString("#" + x.ToString("X")[2..]));
             Scheme<Color> lightSchemeColor = lightScheme.Convert(x => (Color)ColorConverter.ConvertFromString("#" + x.ToString("X")[2..]));
